f6160ce5466b7f27b7d6624098d7f1289c42187d
KIMDJ\vzmzm <azmzm@naver.com>
2025-04-01 16:02:32 +0900

    [dj][2025-04-01] contact 수정

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -5,18 +5,17 @@ const Contact = () => {
         <section id="contact" className="scroll-mt-20 py-28 bg-gray-900 px-6">
             <div className="max-w-xl mx-auto text-center">
                 <h2 className="text-3xl font-semibold text-blue-400 mb-4">Contact</h2>
-                <p className="text-gray-300 mb-2">
-                    🔗 GitHub: <a href="https://github.com/ddj82" className="underline hover:text-blue-300"
-                                 target="_blank" rel="noopener noreferrer">github.com/ddj82</a>
+                <p className="text-gray-300">
+                    📱 휴대폰: <span>010-5064-5726</span>
                 </p>
                 <p className="text-gray-300">
                     📧 이메일: <a href="mailto:dnwnrudckf77@gmail.com"
                               className="underline hover:text-blue-300">dnwnrudckf77@gmail.com</a>
                 </p>
-                <p className="text-gray-300">
-                    {/*📱 휴대폰: <span>010-5064-5726</span>*/}
+                <p className="text-gray-300 mb-2">
+                    🔗 GitHub: <a href="https://github.com/ddj82" className="underline hover:text-blue-300"
+                                 target="_blank" rel="noopener noreferrer">github.com/ddj82</a>
                 </p>
-
             </div>
         </section>
     );
